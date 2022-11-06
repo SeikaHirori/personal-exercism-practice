be4bb6218a366cecabb1d143c54b7fa64246465f
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-05 23:19:41 -0700

    Manually implemented month conversion

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,5 @@
 use time::Date;
 use time::PrimitiveDateTime as DateTime;
-use time::Month;
 use time::Time;
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
@@ -14,9 +13,26 @@ pub fn after(start: DateTime) -> DateTime {
     let conversion_minute = 60;
 
     let mut year = start.year();
-    let mut month = {
-        let current_month = start.month();
-        Month::from(current_month) // This should convert format from Month to u8
+    let mut month:u8 = {
+        let string_month = start.month().to_string();
+        
+        match string_month.as_str() {
+            "January" => 1,
+            "Feburary" => 2,
+            "March" => 3,
+            "April" => 4,
+            "May" => 5,
+            "June" => 6,
+            "July" => 7,
+            "August" => 8,
+            "September" => 9,
+            "October" => 10,
+            "November" => 11,
+            "December" => 12,
+            _ => 0
+        }
+        
+        // This should convert format from Month to u8
     };
     let mut day = start.day();
     let mut hour = start.hour();
@@ -24,7 +40,20 @@ pub fn after(start: DateTime) -> DateTime {
     let mut second = start.second();
 
     // Write logic here
+    let mut total:i32 = {
+        let mut total_value:i32 = 0;
+        
+        total_value += i32::from(year) * conversion_year;
+        total_value += i32::from(month) * conversion_month;
+        total_value += i32::from(day) * conversion_day;
+        total_value += i32::from(hour) * conversion_hour;
+        total_value += i32::from(minute) * conversion_minute;
+        total_value += i32::from(second);
 
+        total_value += gigasecond;
+
+        total_value
+    };
 
     return DateTime::new(
         Date::from_calendar_date(
