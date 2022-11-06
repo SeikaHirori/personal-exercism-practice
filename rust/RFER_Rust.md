e26f187012d1139d6384747e383829b0aae1513c
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-05 21:08:37 -0700

    Rust: Finished reserve_string - Starting gigasecond

---
+++ rust/RFER_Rust.md
@@ -0,0 +1,13 @@
+
+
+# RFER 3
+- Converting vector of chars into a String
+    - https://stackoverflow.com/a/37307684
+
+# RFER 2
+- Reversing iterator
+    - https://stackoverflow.com/a/39775142
+
+# RFER 1
+- Breaking string into a vector of chars
+    - https://stackoverflow.com/a/22894333
--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    let chars_inputs: Vec<char> = input
+        .chars()
+        .rev()
+        .collect();
+    
+    let output_string:String = String::from_iter(chars_inputs);
+
+
+    return String::from(output_string);
 }
