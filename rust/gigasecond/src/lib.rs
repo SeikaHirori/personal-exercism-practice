daf5b1dfeda1871d2dccfbbb997e7eda9752d33c
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-05 22:30:22 -0700

    Started gigaseconds; - Setup variables and return

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,33 @@
+use time::Date;
 use time::PrimitiveDateTime as DateTime;
-
+use time::Month;
+use time::Time;
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let gigasecond:i32 = 1000000000;
+
+    // Conversion: From main format to seconds
+    let conversion_year: i32 = 31556952;
+    let conversion_month: i32 = 2628000;
+    let conversion_day: i32 = 86400;
+    let conversion_hour: i32 = 3600;
+    let conversion_minute = 60;
+
+    let mut year = start.year();
+    let mut month = {
+        let current_month = start.month();
+        Month::from(current_month) // This should convert format from Month to u8
+    };
+    let mut day = start.day();
+    let mut hour = start.hour();
+    let mut minute = start.minute();
+    let mut second = start.second();
+
+    // Write logic here
+
+    return DateTime::new(
+        Date::from_calendar_date(year, month, day).unwrap(), 
+        Time::from_hms(hour, minute, second).unwrap(),
+    )
+
 }
