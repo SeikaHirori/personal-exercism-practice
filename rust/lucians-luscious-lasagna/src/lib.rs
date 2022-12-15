f2f596f9efa02173029094bb36fc643c84ca73f7
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-15 14:18:27 -0800

    Passed all local tests

--- rust/lucians-luscious-lasagna/src/lib.rs
+++ rust/lucians-luscious-lasagna/src/lib.rs
@@ -22,16 +22,31 @@ pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
 }
 
 pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
-    unimplemented!(
-        "calculate preparation time in minutes for number of layers: {}",
-        number_of_layers
-    )
+    // unimplemented!(
+    //     "calculate preparation time in minutes for number of layers: {}",
+    //     number_of_layers
+    // )
+    let layer_per_minute:i32 = 2;
+
+    let result:i32 = {
+        layer_per_minute * number_of_layers
+    };
+
+    result
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
-        number_of_layers,
-        actual_minutes_in_oven
-    )
-}
+    // unimplemented!(
+    //     "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
+    //     number_of_layers,
+    //     actual_minutes_in_oven
+    // )
+
+    let time_layers:i32 = preparation_time_in_minutes(number_of_layers);
+    
+    let result:i32 = {
+        time_layers + actual_minutes_in_oven
+    };
+
+    result
+} // TODO
