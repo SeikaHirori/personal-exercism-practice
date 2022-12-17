fc5e2727908651952a0dd3316601cbed128a6fc3
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-16 20:42:23 -0800

    Moved and created func success_rate()

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -8,23 +8,28 @@ const CARS_PER_SPEED:f64 = 221.0;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     // unimplemented!("calculate hourly production rate at speed: {}", speed)
-    let success_rate:f64 = {
-        if speed >= 9 {
-            0.77
-        } else if speed >= 5 {
-            0.90
-        } else {
-            1.00
-        }
-    };
+    let success_rate:f64 = success_rate(speed);
 
     let result:f64 = {
         f64::from(speed) * CARS_PER_SPEED * success_rate
     };
 
     result
-} // TODO
+}
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
     unimplemented!("calculate the amount of working items at speed: {}", speed)
+
+
+
 } // TODO
+
+pub fn success_rate(speed: u8) -> f64 {
+    if speed >= 9 {
+        0.77
+    } else if speed >= 5 {
+        0.90
+    } else {
+        1.00
+    }
+}
