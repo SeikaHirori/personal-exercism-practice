30d0841894eb23bd12a0cef80ef3dd559ebe6d56
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-12-22 13:28:02 -0800

    FAILED: Allergies

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,38 +1,62 @@
 
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Allergen {
-    Eggs = 0,
-    Peanuts = 1,
-    Shellfish = 2,
-    Strawberries = 3,
-    Tomatoes = 4,
-    Chocolate = 5,
-    Pollen = 6,
-    Cats = 7,
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
 }
 
-pub fn convert_to_exponent(item_index:isize) -> u32 {
-    2u32.pow(item_index as u32)
-}
 
-pub struct Allergies;
+
+pub struct Allergies{
+    total_score:u32,
+}
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        // unimplemented!(
+        //     "Given the '{}' score, construct a new Allergies struct.",
+        //     score
+        // );
+        let mut modified_value = score;
+
+        while modified_value >= 256 {
+            modified_value -= 256;
+        };
+
+        Self {
+            total_score: modified_value
+        }
+
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
-    }
+        // unimplemented!(
+        //     "Determine if the patient is allergic to the '{:?}' allergen.",
+        //     allergen
+        // );
+
+    } // TODO
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        // unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        let output: Vec<Allergen> = Vec::new();
+
+        let value = &self.total_score;
+
+        output
     }
+
+    pub fn allergy_list(&self) -> Vec<Allergy> {
+        
+    }
+
+
+
 }
