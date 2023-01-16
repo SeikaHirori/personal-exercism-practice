1ba0cac8996172280888f286c84c5711463babaa
WaToArt <93100367+WaToArt@users.noreply.github.com>
2023-01-16 15:31:44 -0800

    Startred Swift's Freelance Rates
    Implemented func: dailyRates and monthlyRates

--- swift/freelancer-rates/Sources/FreelancerRates/FreelancerRates.swift
+++ swift/freelancer-rates/Sources/FreelancerRates/FreelancerRates.swift
@@ -1,13 +1,31 @@
+let dailyRate:Int = 8
+let billableDaysInMonth: Int = 22
+
 func dailyRateFrom(hourlyRate: Int) -> Double {
   // fatalError("Please implement the dailyRateFrom(hourlyRate:) function")
+    let output = Double(dailyRate * hourlyRate)
     
+    return output
     
 }
 
 func monthlyRateFrom(hourlyRate: Int, withDiscount discount: Double) -> Double {
-  fatalError("Please implement the monthlyRateFrom(hourlyRate:withDiscount:) function")
+  // fatalError("Please implement the monthlyRateFrom(hourlyRate:withDiscount:) function")
+    var output:Double
+    
+    let dailyTotal = dailyRateFrom(hourlyRate: hourlyRate)
+    
+    let discountTotal = dailyTotal * discount
+    
+    let totalPostDiscount = dailyTotal - discountTotal
+    
+    output = totalPostDiscount * Double(billableDaysInMonth)
+    
+    return output
 }
 
 func workdaysIn(budget: Double, hourlyRate: Int, withDiscount discount: Double) -> Double {
-  fatalError("Please implement the workdaysIn(budget:hourlyRate:withDiscount:) function")
+  // fatalError("Please implement the workdaysIn(budget:hourlyRate:withDiscount:) function")
+    
+    
 }
