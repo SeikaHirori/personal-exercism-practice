3cf6ec7bc8ba57e7115878cf06b740ab39e5615f
WaToArt <93100367+WaToArt@users.noreply.github.com>
2023-01-18 17:47:52 -0800

    Started Swift's Cal's Custom Sign Shop and implemented constants and func BuildSign

---
+++ swift/custom-signs/Sources/CustomSigns/CustomSigns.swift
@@ -0,0 +1,46 @@
+// TODO: define the 'birthday' String constant
+let birthday:String = "Birthday"
+// TODO: define the 'valentine' String constant
+let valentine:String = "Valentine's Day"
+// TODO: define the 'anniversary' String constant
+let anniversary:String = "Anniversary"
+// TODO: define the 'space' Character constant
+let space: Character = " "
+// TODO: define the 'exclamation' Character constant
+let exclamation:Character = "!"
+
+func buildSign(for occasion: String, name: String) -> String {
+    // fatalError("Please implement the buildSign(for:name:) function")
+    var output:String = "Happy" + String(space)
+    
+    switch occasion {
+    case birthday:
+        output += birthday
+    case valentine:
+        output += valentine
+    case anniversary:
+        output += anniversary
+    default:
+        break
+    }
+    
+    output += String(space)
+    
+    output += name
+    
+    output += String(exclamation)
+    
+    return output
+}
+
+func graduationFor(name: String, year: Int) -> String {
+  fatalError("Please implement the graduationFor(name:year:) function")
+    
+    
+}
+
+func costOf(sign: String) -> Int {
+    fatalError("Please implement the costOf(sign:) function")
+
+    
+}
