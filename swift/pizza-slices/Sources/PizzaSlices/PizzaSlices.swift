bf3ca077b2603c2a053fc8b25bd6bc4a0ebd07d3
SeikaHirori <SeikaHirori@outlook.com>
2023-01-23 14:21:18 -0800

    Working on func "SliceSize" Swift's PizzaSlices

---
+++ swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
@@ -0,0 +1,17 @@
+func sliceSize(diameter: Double?, slices: Int?) -> Double? {
+    // fatalError("Please implement the sliceSize(diameter:slices:) function")
+    guard let unwrapDiameter:Double = diameter else {return nil}
+    guard let unwrapSlices:Int = slices else {return nil}
+    
+    var output:Double? = 0.0
+    
+    return output
+}
+    
+
+func biggestSlice(
+  diameterA: String, slicesA: String,
+  diameterB: String, slicesB: String
+) -> String {
+  fatalError("Please implement the biggestSlice(diameterA:slicesA:diameterB:slicesB:) function")
+}
