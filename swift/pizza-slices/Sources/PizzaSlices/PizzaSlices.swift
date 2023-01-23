ff18ee9aa1a3fed34c8c818a03a5f2716ac665de
SeikaHirori <SeikaHirori@outlook.com>
2023-01-23 15:18:10 -0800

    current revision is broken; unsure if the conditions are being meant

--- swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
+++ swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
@@ -2,8 +2,17 @@ import Darwin
 
 func sliceSize(diameter: Double?, slices: Int?) -> Double? {
     // fatalError("Please implement the sliceSize(diameter:slices:) function")
-    guard let unwrapDiameter:Double = diameter else {return nil}
-    guard let unwrapSlices:Int = slices else {return nil}
+    guard let unwrapDiameter:Double = diameter else {
+        return nil}
+    guard let unwrapSlices:Int = slices else {
+        return nil}
+    
+    guard unwrapDiameter >= 0 else {
+        return nil
+    }
+    guard unwrapSlices >= 1 else {
+        return nil
+    }
     
     
     var output:Double = 0.0
@@ -28,5 +37,24 @@ func biggestSlice(
 //    guard let unwrappedDiameterA = Double (diameterA) else {
 //        let unwrappedDiameterA:Double? = nil
 //    }
+    let convertDiameterA:Double? = Double(diameterA)
+    let convertDiameterB:Double? = Double(diameterB)
     
+    if convertDiameterA == nil && convertDiameterB == nil {
+        return "Neither slice is bigger"
+    }
+    
+    if convertDiameterB == nil  {
+        return "Slice A is bigger."
+    } else if convertDiameterA == nil {
+        return "Slice B is bigger."
+    }
+    
+    
+    
+    var output:String = ""
+    
+    
+    
+    return output
 }
