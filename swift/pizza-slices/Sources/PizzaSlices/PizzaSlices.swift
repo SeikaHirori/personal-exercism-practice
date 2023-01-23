fd74cf56b5d88f5d2381bc2dd982b4c05fc56c44
SeikaHirori <SeikaHirori@outlook.com>
2023-01-23 14:47:05 -0800

    Finished func sliceSize()

--- swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
+++ swift/pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
@@ -1,9 +1,18 @@
+import Darwin
+
 func sliceSize(diameter: Double?, slices: Int?) -> Double? {
     // fatalError("Please implement the sliceSize(diameter:slices:) function")
     guard let unwrapDiameter:Double = diameter else {return nil}
     guard let unwrapSlices:Int = slices else {return nil}
     
-    var output:Double? = 0.0
+    
+    var output:Double = 0.0
+    
+    let radius:Double = unwrapDiameter / 2.0
+
+    let area = Double.pi * pow(radius, 2)
+    
+    output = area / Double(unwrapSlices)
     
     return output
 }
