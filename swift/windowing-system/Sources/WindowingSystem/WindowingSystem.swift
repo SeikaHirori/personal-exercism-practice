71844e85b524595a489db916100f5de56c364769
SeikaHirori <SeikaHirori@outlook.com>
2023-01-29 17:07:21 -0800

    defined struct: - Position() defined class: - Window()

--- swift/windowing-system/Sources/WindowingSystem/WindowingSystem.swift
+++ swift/windowing-system/Sources/WindowingSystem/WindowingSystem.swift
@@ -1,4 +1,15 @@
 // TODO: Define the Position struct
+struct Position {
+    var x:Int = 0
+    var y:Int = 0
+    
+    mutating func moveTo(newX: Int, newY: Int) {
+        self.x = newX
+        self.y = newY
+    }
+}
+
+// TODO: Define the Size struct
 struct Size {
     var width:Int = 80
     var height: Int = 60
@@ -10,6 +21,12 @@ struct Size {
     
 }
 
-// TODO: Define the Size struct
-
 // TODO: Define the Window class
+class Window {
+    var title:String = "New Window"
+    let screenSize:Size = Size(width: 800, height: 600)
+    var size:Size = Size()
+    var position:Position = Position()
+    var contents:String? = nil
+    
+}
