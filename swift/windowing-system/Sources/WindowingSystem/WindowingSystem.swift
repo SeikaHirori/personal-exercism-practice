a87612ac3f9de9614efad5bc04355df9fcb65e8e
SeikaHirori <SeikaHirori@outlook.com>
2023-01-29 16:19:17 -0800

    Started Swift's Windowing System and wrote struct: - Size

---
+++ swift/windowing-system/Sources/WindowingSystem/WindowingSystem.swift
@@ -0,0 +1,15 @@
+// TODO: Define the Position struct
+struct Size {
+    var width:Int = 80
+    var height: Int = 60
+    
+    mutating func resize(newWidth: Int, newHeight:Int) {
+        self.width = newWidth
+        self.height = newHeight
+    }
+    
+}
+
+// TODO: Define the Size struct
+
+// TODO: Define the Window class
