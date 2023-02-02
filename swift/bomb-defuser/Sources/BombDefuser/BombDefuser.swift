fa82f67e6b7eec9fdb39c8a62cd4d0673d7c9eed
SeikaHirori <SeikaHirori@outlook.com>
2023-02-02 14:08:42 -0800

    wrote func flip(), but needs to be tested later

--- swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
+++ swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
@@ -1,7 +1,15 @@
+import Foundation
+
 //let flip: (String, String, String) -> (String, String, String) = TODO: Please define the flip closure
+let flip = { (a:(String, String, String)) -> ((String, String, String)) in
+    return (a.1, a.0, a.2)
+}
+
+
 //
 //
 //let rotate: (String, String, String) -> (String, String, String) = TODO: Please define the stopAtOne closure
+
 
 func makeShuffle(
   flipper: @escaping ((String, String, String)) -> (String, String, String),
