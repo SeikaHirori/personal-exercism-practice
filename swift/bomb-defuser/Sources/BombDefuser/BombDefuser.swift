60b0e5f84fe94ae3ba86113193d678b798f0e2ce
SeikaHirori <SeikaHirori@outlook.com>
2023-02-02 15:21:13 -0800

    currently only failing test:
    - testShuffle1()

--- swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
+++ swift/bomb-defuser/Sources/BombDefuser/BombDefuser.swift
@@ -18,5 +18,48 @@ func makeShuffle(
   flipper: @escaping ((String, String, String)) -> (String, String, String),
   rotator: @escaping ((String, String, String)) -> (String, String, String)
 ) -> (UInt8, (String, String, String)) -> (String, String, String) {
-  fatalError("Please implement the makeShuffle(flipper:rotator:) function")
+//  fatalError("Please implement the makeShuffle(flipper:rotator:) function")
+    var closedSyn: (UInt8, (String, String, String)) -> (String, String, String) = {
+        var bitArray:[Int] = []
+        
+        var copyUInt8:Int = Int($0)
+        
+        while copyUInt8 > 0 {
+            let dividendCopy = copyUInt8 / 2
+            let remainderCopy = copyUInt8 % 2
+            
+            bitArray.append(remainderCopy)
+            
+            let debugCurrentCycle: String = """
+            copyUInt8: \(copyUInt8)
+            dividendCopy: \(dividendCopy)
+            remaidnerCopy: \(remainderCopy)
+            """
+            print(debugCurrentCycle)
+            
+            copyUInt8 = dividendCopy
+            
+        }
+        
+        bitArray = bitArray.reversed()
+        
+        var copyTuple:(String, String, String) = $1
+        
+        while bitArray.isEmpty != true {
+            let currentBit = bitArray.popLast()
+            
+            switch currentBit {
+            case 0:
+                copyTuple = flipper(copyTuple)
+            case 1:
+                copyTuple = rotator(copyTuple)
+            default:
+                continue
+            }
+        }
+        return copyTuple
+        
+    }
+    
+    return closedSyn
 }
