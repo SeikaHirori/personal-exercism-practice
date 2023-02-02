ca6a2d54fc21dd84f566f03a4eb4584b9016f79a
SeikaHirori <SeikaHirori@outlook.com>
2023-02-01 19:02:24 -0800

    Passed all tests for all funcs and enums

--- swift/log-lines/Sources/LogLines/LogLines.swift
+++ swift/log-lines/Sources/LogLines/LogLines.swift
@@ -1,3 +1,45 @@
 // TODO: define the 'LogLevel' enum, its `init`, and its `shortFormat` method
+import Foundation
 
-
+enum LogLevel:String {
+    case trace = "0"
+    case debug = "1"
+    case info = "4"
+    case warning = "5"
+    case error = "6"
+    case fatal = "7"
+    case unknown = "42"
+    
+    init(_ message: String) {
+        
+        let arrayOfMessage:[String] = message.components(separatedBy: ":")
+        
+        let codeName:String = arrayOfMessage[0]
+        
+        switch codeName {
+        case "[TRC]":
+            self = .trace
+        case "[DBG]":
+            self = .debug
+        case "[INF]":
+            self = .info
+        case "[WRN]":
+            self = .warning
+        case "[ERR]":
+            self = .error
+        case "[FTL]":
+            self = .fatal
+        default:
+            self = .unknown
+        }
+    }
+    
+    func shortFormat(message: String) -> String {
+        let output:String = "\(rawValue):\(message)"
+        
+        return output
+    }
+    
+    
+    
+}
