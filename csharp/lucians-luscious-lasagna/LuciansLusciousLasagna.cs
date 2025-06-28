d4617427f3a3c4894fa7c7608eeffafd85987faa
SeikaHirori <93100367+SeikaHirori@users.noreply.github.com>
2025-06-28 07:59:36 -0700

    finished lucians

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,28 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int x)
+    {
+        return ExpectedMinutesInOven() - x;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int x)
+    {
+        return 2 * x;
+    }
+
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int overTime)
+    {
+        return PreparationTimeInMinutes(layers) +
+            overTime;
+    }
 }
