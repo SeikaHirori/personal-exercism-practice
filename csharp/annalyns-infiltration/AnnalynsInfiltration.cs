8ef163b085e274186deb0d0a806ecd8820106996
SeikaHirori <93100367+SeikaHirori@users.noreply.github.com>
2025-06-28 08:34:56 -0700

    Finished Annalyns

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -1,22 +1,46 @@
+using System.Reflection.Metadata.Ecma335;
+
 static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        return !knightIsAwake;
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if (petDogIsPresent)
+        {
+            if (!archerIsAwake)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (prisonerIsAwake)
+            {
+                if (!knightIsAwake && !archerIsAwake)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
