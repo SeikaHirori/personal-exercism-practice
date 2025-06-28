fd2347ff3f0801bb67821e3f61332796782647cd
SeikaHirori <93100367+SeikaHirori@users.noreply.github.com>
2025-06-28 08:20:03 -0700

    skipping logslevel

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -1,8 +1,15 @@
+using System.Collections;
+
 static class LogLine
 {
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        var parts = new List<string>(logLine.Split(' '));
+        parts.RemoveAt(0);
+
+        string output = String.Join(' ', parts.ToArray());
+
+        return output;
     }
 
     public static string LogLevel(string logLine)
